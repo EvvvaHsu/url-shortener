25098a9f9da09c687ae4ddfe7af4c9b5b1b81165
Yiching Hsu <ychsu7149@gmail.com>
2023-04-10 00:32:42 +0800

    Add initial setting files. MongoDB and Mongoose are connected.

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+//const mongoose = require("mongoose") 
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const app = express()
+//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+require('./config/mongoose')
+const routes = require('./routes') //預設會去找底下的index.js
+
+// const db = mongoose.connection
+
+// db.on('error', () => {
+//     console.log('mongodb error!')
+// })
+
+// db.once('open', () => {
+//     console.log('mongodb connected!')
+// })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on http://localhost:3000')
+})
