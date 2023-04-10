338921ccf1b017532516115c77fa429f7952d760
Yiching Hsu <ychsu7149@gmail.com>
2023-04-10 22:21:50 +0800

    feat: add generate url file

---
+++ generate_url.js
@@ -0,0 +1,28 @@
+
+function sample(array) {
+    let randomIndex = Math.floor(Math.random() * array.length)
+    return array[randomIndex]
+}
+
+
+function generateURL(){
+
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const lowercaseArray = lowerCaseLetters.split('')
+const upperCaseLetters = lowerCaseLetters.toUpperCase()
+const uppercaseArray = upperCaseLetters.split('')
+const numbers = '1234567890'
+const numbersArray = numbers.split('')
+const collection = lowercaseArray.concat(uppercaseArray, numbersArray)
+let urlRoute = "localhost:3000/"
+for (let i = 0; i < 5; i++) {
+    urlRoute += sample(collection)
+}
+return urlRoute
+}
+
+module.exports = generateURL
+
+//console.log(generateURL())
+
+
